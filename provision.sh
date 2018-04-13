118b4b53254fe4e1d0ef73ff029c5b0796fed7ad
Roaldl <roald.lemmen@ouce.ox.ac.uk>
2018-04-13 14:40:26 +0100

    Target vagrant file to forked repository

--- provision.sh
+++ provision.sh
@@ -1,4 +1,4 @@
 # Install package and restart container
-docker exec -i opencpu-rstudio bash -c "R -e \"remove.packages('josephguillaume/breakdown')\""
-docker exec -i opencpu-rstudio bash -c "R -e \"devtools::install_github('josephguillaume/breakdown')\""
+docker exec -i opencpu-rstudio bash -c "R -e \"remove.packages('roaldl/breakdown')\""
+docker exec -i opencpu-rstudio bash -c "R -e \"devtools::install_github('roaldl/breakdown')\""
 docker exec -i opencpu-rstudio bash -c "R -e \"library(opencpu); ocpu_start_app('breakdown')\""
